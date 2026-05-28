855a973577d61ef5bcf4bcc1815c12a2295ad88f
bobbiswas12 <tanmayrai1729@gmail.com>
2026-05-29 02:32:21 +0530

    sca3-added-noise

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #define screen_width 800
 #define screen_height 800
-#define rows 120
-#define columns 120
+#define rows 200
+#define columns 200
+#define fps 10
 
-
-float random_value(int value){
+float random_value(float value){
 
   return (((float) rand()/(float) RAND_MAX)*(float) value);
 }
@@ -29,26 +29,36 @@ typedef struct{
 
 model update_model(model cells){
 
-  model new;
+  model new = {0};
   for (int i = 0; i < rows; i++){
     for (int j = 0; j < columns; j++){
+      
       if (i == 0 && j == 0){
-	new.cellular[i][j].prob = 1 - cells.cellular[i+1][j].prob * cells.cellular[i][j+1].prob;
+	new.cellular[i][j].prob = (cells.cellular[i+1][j].prob + cells.cellular[i][j+1].prob)/2;
       }
       else if(i== 0 && j == columns - 1){
-	new.cellular[0][columns - 1].prob = 1 - cells.cellular[1][columns - 1].prob*cells.cellular[0][columns - 2].prob;
+	new.cellular[0][columns - 1].prob = (cells.cellular[1][columns - 1].prob*cells.cellular[0][columns - 2].prob)/2;
+      }
+       else if(i == rows - 1 && j == 0){
+	 new.cellular[i][j].prob = (cells.cellular[i-1][j].prob * cells.cellular[i][j+1].prob)/2;
       }
       else if(i == rows - 1 && j == columns - 1){
-	new.cellular[rows - 1][columns - 1].prob = 1 - cells.cellular[rows - 2][columns -1].prob*cells.cellular[rows -1][columns -2].prob;
+	new.cellular[rows - 1][columns - 1].prob = (cells.cellular[rows - 2][columns -1].prob*cells.cellular[rows -1][columns -2].prob)/2;
       }
       else if(i == 0){
-	new.cellular[0][j].prob = 1 - cells.cellular[0][j-1].prob*cells.cellular[0][j+1].prob*cells.cellular[1][j].prob;
+	new.cellular[0][j].prob = (cells.cellular[0][j-1].prob*cells.cellular[0][j+1].prob*cells.cellular[1][j].prob)/3;
+      }
+      else if(i == rows - 1){
+	new.cellular[i][j].prob = (cells.cellular[i][j-1].prob * cells.cellular[i][j+1].prob * cells.cellular[i-1][j].prob)/3;
       }
       else if(j == 0){
-	new.cellular[i][0].prob = 1 - cells.cellular[i-1][0].prob*cells.cellular[i+1][0].prob*cells.cellular[i][1].prob;
+	new.cellular[i][0].prob = (cells.cellular[i-1][0].prob*cells.cellular[i+1][0].prob*cells.cellular[i][1].prob)/3;
+      }
+      else if(j == columns - 1){
+	new.cellular[i][j].prob = (cells.cellular[i-1][j].prob * cells.cellular[i+1][j].prob * cells.cellular[i][j-1].prob)/3;
       }
       else {
-	new.cellular[i][j].prob = 1 - cells.cellular[i-1][j].prob*cells.cellular[i+1][j].prob*cells.cellular[i][j-1].prob*cells.cellular[i][j+1].prob;
+	new.cellular[i][j].prob = (cells.cellular[i-1][j].prob*cells.cellular[i+1][j].prob*cells.cellular[i][j-1].prob*cells.cellular[i][j+1].prob)/4;
       }
     }
   }
@@ -59,11 +69,29 @@ model update_model(model cells){
   }
   return new;
 }
+
+model noise(model cells,float noise_volume){
+
+  for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+
+      cells.cellular[i][j].prob += random_value(noise_volume) - noise_volume/2;
+      if (cells.cellular[i][j].prob < 0){
+	cells.cellular[i][j].prob = 0;
+      }
+      else if(cells.cellular[i][j].prob > 1){
+	cells.cellular[i][j].prob = 1;
+      }
+    }
+  }
+
+  return cells;
+}
 	
 int main(){
 
   InitWindow(screen_width,screen_height,"window");
-  SetTargetFPS(10);
+  SetTargetFPS(fps);
   srand(time(0));
 
   model cells = {0};
@@ -89,30 +117,31 @@ int main(){
     BeginDrawing();
     ClearBackground(BLACK);
 
-    for (int i = 100; i <= screen_width - 100; i+= (screen_width-200)/columns){
+    /* for (int i = 100; i <= screen_width - 100; i+= (screen_width-200)/columns){ */
 
-      Vector2 initial = (Vector2) {i,100};
-      Vector2 final = (Vector2) {i,screen_height - 100};
-      DrawLineV(initial,final,WHITE);
+    /*   Vector2 initial = (Vector2) {i,100}; */
+    /*   Vector2 final = (Vector2) {i,screen_height - 100}; */
+    /*   DrawLineV(initial,final,WHITE); */
 
-    }
+    /* } */
 
-    for (int i = 100; i <= screen_height - 100; i += (screen_height - 200)/rows){
+    /* for (int i = 100; i <= screen_height - 100; i += (screen_height - 200)/rows){ */
 
-      Vector2 initial = (Vector2) {100,i};
-      Vector2 final = (Vector2) {screen_width - 100,i};
-      DrawLineV(initial,final,WHITE);
-    }
+    /*   Vector2 initial = (Vector2) {100,i}; */
+    /*   Vector2 final = (Vector2) {screen_width - 100,i}; */
+    /*   DrawLineV(initial,final,WHITE); */
+    /* } */
 
     for (int i = 0; i < rows; i++){
       for (int j = 0; j < columns; j++){
-	if (cells.cellular[i][j].state && cells.cellular[i][j].prob >= 0.0003){
+	if (cells.cellular[i][j].state && cells.cellular[i][j].prob >= 0.01){
 	  Vector2 top_left = (Vector2) {100 + ((screen_width-200)/columns) * j, 100 + ((screen_height - 200)/rows) * i};
-	  DrawRectangleV(top_left,(Vector2) {(screen_width-200)/columns,(screen_height - 200)/rows},BLACK);
+	  DrawRectangleV(top_left,(Vector2) {(screen_width-200)/columns,(screen_height - 200)/rows},WHITE);
 	}
       }
     }
     cells = update_model(cells);
+    cells = noise(cells,0.05);
     DrawFPS(10,10);
     DrawText(TextFormat("Gen: %d", stage),10,50,30,RED);
     stage++;
